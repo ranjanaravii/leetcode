ef0f0fd7fad002635f4bdb9501841cb4ae7b355b
raviranjankumar <raviranjan.kumar@lenskart.com>
2025-07-28 15:50:39 +0530

    containsNearbyDuplicate

--- TwoPointer/BoatToSavePeople.java
+++ TwoPointer/BoatToSavePeople.java
@@ -2,6 +2,8 @@
 package TwoPointer;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BoatToSavePeople {
 
@@ -17,5 +19,18 @@ public class BoatToSavePeople {
             right--;
         }
         return boats;
+    }
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                if (Math.abs(map.get(nums[i]) - i) <= k) {
+                    return true;
+                }
+            }
+            map.put(nums[i], i);
+        }
+        return false;
     }
 }
