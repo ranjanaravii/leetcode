56f9cbc09e787e8a63aee125d4f48837f601065f
raviranjankumar <raviranjan.kumar@lenskart.com>
2025-07-31 00:02:32 +0530

    interval

---
+++ Interval/Interval.java
@@ -0,0 +1,10 @@
+package Interval;
+
+public class Interval {
+    public int start, end;
+
+    public Interval(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+}
---
+++ Interval/MeetingRoom.java
@@ -0,0 +1,47 @@
+package Interval;
+
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class MeetingRoom {
+
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        intervals.sort((a, b) -> a.start - b.start);
+        for (int i = 0; i < intervals.size() - 1; i++) {
+            if (intervals.get(i).end > intervals.get(i + 1).start) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int minMeetingRooms(List<Interval> intervals) {
+        if (intervals == null || intervals.size() == 0)
+            return 0;
+
+        // Sort by start time
+        intervals.sort((a, b) -> a.start - b.start);
+
+        // Min-heap based on meeting end times
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (Interval interval : intervals) {
+            // If room is free, remove it from the heap
+            if (!minHeap.isEmpty() && interval.start >= minHeap.peek()) {
+                minHeap.poll(); // reuse the room
+            }
+            // Add the current meeting's end time to the heap
+            minHeap.offer(interval.end);
+        }
+
+        // The heap size is the number of rooms used
+        return minHeap.size();
+    }
+
+    public static void main(String[] args) {
+        MeetingRoom meetingRoom = new MeetingRoom();
+        List<Interval> intervals = List.of(new Interval(0, 40), new Interval(5, 10), new Interval(15, 20));
+        // System.out.println(meetingRoom.canAttendMeetings(intervals));
+        System.out.println(meetingRoom.minMeetingRooms(intervals));
+    }
+}
