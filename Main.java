0fdb7080c090b32f0226abbd998314a355fda488
ranjanaravii <raviranjanroyrrr1@gmail.com>
2025-08-12 00:33:29 +0530

    test

--- Main.java
+++ Main.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 4, 5};
@@ -7,6 +11,28 @@ public class Main {
     public static void printArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
+        }
+    }
+
+    static class Result {
+        public static List<Integer> getValueAtIndices(List<Integer> query) {
+            int maxIndex = Collections.max(query);
+            List<Integer> sequence = new ArrayList<>();
+            int num = 1;
+
+            while (sequence.size() <= maxIndex) {
+                int ones = Integer.bitCount(num);
+                for (int i = 0; i < ones && sequence.size() <= maxIndex; i++) {
+                    sequence.add(num);
+                }
+                num++;
+            }
+
+            List<Integer> result = new ArrayList<>();
+            for (int idx : query) {
+                result.add(sequence.get(idx));
+            }
+            return result;
         }
     }
 }
