8b10536f287a9a46dfe3cdae53abbb0e778141b9
ranjanaravii <raviranjanroyrrr1@gmail.com>
2025-09-03 00:47:17 +0530

    clone graph

--- Graph/Node.java
+++ Graph/Node.java
@@ -1,10 +1,28 @@
 package Graph;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Node {
     int key, value;
     Node next, prev;
+    public List<Node> neighbors;
     Node(int key, int value){
         this.key = key;
         this.value = value;
+    }
+    public Node() {
+        value = 0;
+        neighbors = new ArrayList<>();
+    }
+
+    public Node(int _val) {
+        value = _val;
+        neighbors = new ArrayList<Node>();
+    }
+
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        value = _val;
+        neighbors = _neighbors;
     }
 }
