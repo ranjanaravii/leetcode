fb0f961cb2ced60e61a1230585e140700cc4b2e3
ranjanaravii <raviranjanroyrrr1@gmail.com>
2025-09-10 23:03:36 +0530

    merge sorted linked list

--- Heap/Heap.java
+++ Heap/Heap.java
@@ -222,6 +222,27 @@ public class Heap {
         return res;
     }
 
+    /*************************************************************************************************/
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        List<Integer> nodes = new ArrayList<>();
+        for (ListNode lst : lists) {
+            while (lst != null) {
+                nodes.add(lst.val);
+                lst = lst.next;
+            }
+        }
+        Collections.sort(nodes);
+
+        ListNode res = new ListNode(0);
+        ListNode cur = res;
+        for (int node : nodes) {
+            cur.next = new ListNode(node);
+            cur = cur.next;
+        }
+        return res.next;
+    }
+
     public static void main(String[] args) {
         Heap heap = new Heap();
         int[] nums = { 3, 2, 1, 5, 6, 4 };
